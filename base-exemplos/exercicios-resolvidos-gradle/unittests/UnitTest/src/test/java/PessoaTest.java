50126d918e329381f9f06da8b04967a4cd1c981c
Fernando Avellar <favellar@yahoo.com.br>
2017-09-16 17:08:32 -0300

    Exercicios realizados em sala

--- base-exemplos/exercicios-resolvidos-gradle/unittests/UnitTest/src/test/java/PessoaTest.java
+++ base-exemplos/exercicios-resolvidos-gradle/unittests/UnitTest/src/test/java/PessoaTest.java
@@ -1,4 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
 
 public class PessoaTest {
-
+	
+	@Test
+	public void test() {
+	 Pessoa pessoa = new Pessoa("João", 34);
+	 Assert.assertTrue("A pessoa deve chamar João", pessoa.getName().equals("João"));
+	 Assert.assertTrue("João deve ter 34", pessoa.getIdade() == 34);
+	}
+	
+	@Test
+	public void test1() {
+	 Pessoa pessoa = new Pessoa("Fernando", 37);
+	 Assert.assertFalse("A pessoa não deve chamar João", pessoa.getName().equals("João"));
+	 Assert.assertFalse("Fernando não deve ter 34 anos", pessoa.getIdade() == 34);
+	}
 }
